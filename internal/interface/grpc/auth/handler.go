920d29d34f43bb23401f5b5e5e6931670cd07e94
qkitzero <qkitzero@gmail.com>
2024-12-26 17:07:26 +0900

    add: keycloak

--- internal/interface/grpc/auth/handler.go
+++ internal/interface/grpc/auth/handler.go
@@ -2,27 +2,36 @@ package auth
 
 import (
 	"context"
+	"token/internal/application/auth"
 	"token/internal/application/token"
 	"token/pb"
 )
 
 type AuthHandler struct {
 	pb.UnimplementedAuthServiceServer
+	authService  auth.AuthService
 	tokenService token.TokenService
 }
 
-func NewAuthHandler(tokenService token.TokenService) *AuthHandler {
+func NewAuthHandler(authService auth.AuthService, tokenService token.TokenService) *AuthHandler {
 	return &AuthHandler{
+		authService:  authService,
 		tokenService: tokenService,
 	}
 }
 
 func (h *AuthHandler) GetAccessToken(ctx context.Context, req *pb.GetAccessTokenRequest) (*pb.GetAccessTokenResponse, error) {
-	_, err := h.tokenService.CreateTokne()
+	tokenResponse, err := h.authService.Tokne(req.GetCode())
 	if err != nil {
 		return nil, err
 	}
+
+	_, err = h.tokenService.CreateTokne()
+	if err != nil {
+		return nil, err
+	}
+
 	return &pb.GetAccessTokenResponse{
-		AccessToken: "access token",
+		AccessToken: tokenResponse.AccessToken,
 	}, nil
 }
