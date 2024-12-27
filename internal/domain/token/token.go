dfaac36c2712177cc9ff18cc1f41ac4b43b6bfb3
qkitzero <qkitzero@gmail.com>
2024-12-27 15:00:40 +0900

    fix: token

--- internal/domain/token/token.go
+++ internal/domain/token/token.go
@@ -1,29 +1,66 @@
 package token
 
-import "time"
+import (
+	"time"
+	"token/internal/domain/user"
+)
 
 type Token interface {
 	ID() TokenID
+	AccessToken() string
+	RefreshToken() string
+	UserID() user.UserID
 	CreatedAt() time.Time
+	UpdatedAt() time.Time
+	Update(accessToken, refreshToken string)
 }
 
 type token struct {
-	id        TokenID
-	createdAt time.Time
+	id           TokenID
+	accessToken  string
+	refreshToken string
+	userID       user.UserID
+	createdAt    time.Time
+	updatedAt    time.Time
 }
 
 func (t token) ID() TokenID {
 	return t.id
 }
 
+func (t token) AccessToken() string {
+	return t.accessToken
+}
+
+func (t token) RefreshToken() string {
+	return t.refreshToken
+}
+
+func (t token) UserID() user.UserID {
+	return t.userID
+}
+
 func (t token) CreatedAt() time.Time {
 	return t.createdAt
 }
 
-func NewToken(id TokenID) Token {
-	now := time.Now()
-	return token{
-		id:        id,
-		createdAt: now,
+func (t token) UpdatedAt() time.Time {
+	return t.updatedAt
+}
+
+func (t *token) Update(accessToken, refreshToken string) {
+	t.accessToken = accessToken
+	t.refreshToken = refreshToken
+	t.updatedAt = time.Now()
+}
+
+func NewToken(id TokenID, accessToken, refreshToken string, userID user.UserID, createdAt, updatedAt time.Time) Token {
+	return &token{
+		id:           id,
+		accessToken:  accessToken,
+		refreshToken: refreshToken,
+		userID:       userID,
+		createdAt:    createdAt,
+		updatedAt:    updatedAt,
 	}
 }
